84fc69058bfd0f9bcbd6cd48f1c8fd3333b59d8c
marcelfonteles <marcelfonteles@gmail.com>
2022-12-31 15:17:37 -0300

    connection with mongodb

---
+++ server/src/database.py
@@ -0,0 +1,14 @@
+from pymongo import MongoClient, ASCENDING, DESCENDING
+
+
+def get_database():
+    client = MongoClient('localhost', 27017, username='root', password='password')
+    return client.training
+
+
+def get_last_global_model():
+    client = get_database()
+    training = client.training
+    global_models = training.global_models
+    records = [record for record in global_models.find().sort('createdAt', ASCENDING)]
+    client.close()
